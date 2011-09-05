0c97def5d8be1a494fc12b7dec5439b97f86b0b8
magnomp <magnomp@gmail.com>
2011-09-05 15:04:23 -0300

    Removed IParameter in favour of TValue

--- Src/Emballo.DynamicProxy.InvokationHandler.pas
+++ Src/Emballo.DynamicProxy.InvokationHandler.pas
@@ -24,47 +24,12 @@ uses
   Rtti, SysUtils;
 
 type
-  EParameterReadOnly = class(Exception)
-  public
-    constructor Create;
-  end;
-
-  IParameter = interface
-    ['{480F5D55-BDF1-4ED0-B106-E64AFF13F47E}']
-    function GetAsByte: Byte;
-    procedure SetAsByte(Value: Byte);
-    function GetAsInteger: Integer;
-    procedure SetAsInteger(Value: Integer);
-    function GetAsDouble: Double;
-    procedure SetAsDouble(Value: Double);
-    function GetAsString: String;
-    procedure SetAsString(const Value: String);
-    function GetAsBoolean: Boolean;
-    procedure SetAsBoolean(Value: Boolean);
-    function GetAsDateTime: TDateTime;
-    procedure SetAsDateTime(Value: TDateTime);
-
-    property AsByte: Byte read GetAsByte write SetAsByte;
-    property AsInteger: Integer read GetAsInteger write SetAsInteger;
-    property AsDouble: Double read GetAsDouble write SetAsDouble;
-    property AsString: String read GetAsString write SetAsString;
-    property AsBoolean: Boolean read GetAsBoolean write SetAsBoolean;
-    property AsDateTime: TDateTime read GetAsDateTime write SetAsDateTime;
-  end;
-
   TInvokationHandlerAnonMethod = reference to procedure(const Method: TRttiMethod;
-    const Parameters: TArray<IParameter>; const Result: IParameter);
+    const Parameters: TArray<TValue>; out Result: TValue);
 
   TInvokationHandlerMethod = procedure(const Method: TRttiMethod;
-    const Parameters: TArray<IParameter>; const Result: IParameter) of object;
+    const Parameters: TArray<TValue>; out Result: TValue) of object;
 
 implementation
-
-{ EParameterReadOnly }
-
-constructor EParameterReadOnly.Create;
-begin
-  inherited Create('Parameter is readonly');
-end;
 
 end.

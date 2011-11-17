4d08648ea25db2e1cda8756821940d2f080ef786
magnomp <magnomp@gmail.com>
2011-11-17 21:29:34 -0200

    Moved Emballo.Mock.MockService functionality to Emballo.Services unit

--- Src/Emballo.Services.pas
+++ Src/Emballo.Services.pas
@@ -1,14 +1,33 @@
+{   Copyright 2010 - Magno Machado Paulo (magnomp@gmail.com)
+
+    This file is part of Emballo.
+
+    Emballo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    Emballo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Emballo.
+    If not, see <http://www.gnu.org/licenses/>. }
+
 unit Emballo.Services;
 
 interface
 
 uses
-  Emballo.DI;
+  Emballo.DI, Emballo.Mock.Mock;
 
 type
   EmballoServices = class
   public
     class function CreateInjector(const Modules: array of TModule): TInjector;
+    class function Mock<T:class>: TMock<T>;
   end;
 
 implementation
@@ -18,6 +37,11 @@ implementation
 class function EmballoServices.CreateInjector(const Modules: array of TModule): TInjector;
 begin
   Result := TInjector.Create(Modules);
+end;
+
+class function EmballoServices.Mock<T>: TMock<T>;
+begin
+  Result := TMock<T>.Create;
 end;
 
 end.
